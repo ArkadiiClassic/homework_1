71523167e7d5d2e279ff0b52b1df4c7d1d8c4164
arkadii <arkadii.classsic@gmail.com>
2021-07-28 16:47:51 +0300

    add example_1

--- example/Program.cs
+++ example/Program.cs
@@ -6,7 +6,34 @@ namespace example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Домашняя работа №1
+            /* 1. Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). 
+            В результате вся информация выводится в одну строчку:
+                а) используя склеивание;
+                б) используя форматированный вывод;
+                в) используя вывод со знаком $.*/
+
+            Console.Write("Введите своё имя: ");
+            var name = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите свою фамилию: ");
+            var surname = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите свой возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите свой рост: ");
+            var growth = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите свой вес: ");
+            var weight = Convert.ToDouble(Console.ReadLine());
+
+            //использование склеивания
+            Console.WriteLine("Имя: " + name + ", фамилия: " + surname + ", возраст: "
+                + age + ", рост: " + growth + " и вес:" + weight + ";");
+
+            //использование форматированного вывода
+            Console.WriteLine("{0}, {1}, {2:D}, {3:F2}, {4:F2}",
+                name, surname, age, growth, weight);
+
+            //использование вывода со знаком $.
+            Console.WriteLine($"{name}, {surname}, {age}, {growth}, {weight}");
         }
     }
 }
