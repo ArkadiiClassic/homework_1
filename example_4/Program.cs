df9c329dc16de9b43ab8ec87223250044cca9b31
arkadii <arkadii.classsic@gmail.com>
2021-07-29 16:11:13 +0300

    change example_4

--- example_4/Program.cs
+++ example_4/Program.cs
@@ -6,7 +6,28 @@ namespace example_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Домашняя работа №1 пример_4
+            // Архиреев Аркадий 
+            /* 2. Написать программу обмена значениями двух переменных:
+                а) с использованием третьей переменной;
+	            б) *без использования третьей переменной.*/
+            Console.Write("Введите a: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            // а
+            int c = a;
+            a = b;
+            b = c;
+
+            // б
+            /*a = a + b;
+            b = b - a;
+            b = -b;
+            a = a - b;*/
+
+            Console.Write($"Число a: {a}, число b: {b};");
         }
     }
 }
