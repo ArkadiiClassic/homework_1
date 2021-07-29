188e388ad704bfb298ca95af6f7a3b7e804bfb39
arkadii <arkadii.classsic@gmail.com>
2021-07-29 13:09:50 +0300

    add example_3

---
+++ Example_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Домашняя работа №1 пример_3
+            // Архиреев Аркадий 
+            /* 3. а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2
+                по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). Вывести результат,
+                используя спецификатор формата .2f (с двумя знаками после запятой);
+                  б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.*/
+
+            Console.Write("Введите x1: ");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y1: ");
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите x2: ");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y2: ");
+            var y2 = Convert.ToDouble(Console.ReadLine());
+
+            var r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Результат: {r:f2};");
+        }
+    }
+}
